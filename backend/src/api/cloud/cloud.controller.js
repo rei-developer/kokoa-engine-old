ff92d48a5cb6bae31c938862e9568977ea02f9e6
nakochan <eunnamu094@gmail.com>
2019-05-04 21:32:11 +0900

    server

--- backend/src/api/cloud/cloud.controller.js
+++ backend/src/api/cloud/cloud.controller.js
@@ -1,22 +1,22 @@
 const fs = require('fs')
-//const sharp = require('sharp')
+const sharp = require('sharp')
 const { execFile } = require('child_process')
-//const giflossy = require('giflossy')
+const giflossy = require('giflossy')
 
 module.exports.createImage = type => async ctx => {
   const checker = /(.gif|.png|.jpg|.jpeg|.webp)/i.test(ctx.req.file.originalname)
-  //const checkerByGIF = /.gif/i.test(ctx.req.file.originalname)
+  const checkerByGIF = /.gif/i.test(ctx.req.file.originalname)
   const filename = ctx.req.file.filename
   try {
     if (checker) {
       fs.readFile(`./img/${filename}`, (err, data) => {
         if (err) return ctx.body = { message: err, status: 'fail' }
-        /*if (checkerByGIF) {
+        if (checkerByGIF) {
           execFile(giflossy, ['-O3', '--lossy=80', '-o', `./img/${filename}`, `./img/${filename}`], err => {
             if (err) return ctx.body = { message: err, status: 'fail' }
           })
         } else {*/
-          /*const image = sharp(data)
+          const image = sharp(data)
           if (type === 'topic') {
             image.metadata()
               .then((metadata) => image.resize(Math.min(metadata.width, 960))
@@ -30,16 +30,16 @@ module.exports.createImage = type => async ctx => {
                 .toBuffer()
               )
               .then(result => fs.writeFile(`./img/${filename}`, result, () => { }))
-          }*/
-        //}
-        /*if (type === 'topic') {
+          }
+        }
+        if (type === 'topic') {
           const thumbnail = sharp(data)
           thumbnail.metadata()
             .then(() => thumbnail.resize(100, 100)
               .toBuffer()
             )
             .then(result => fs.writeFile(`./img/thumb/${filename}`, result, () => { }))
-        }*/
+        }
       })
       ctx.body = { filename, status: 'ok' }
     } else {
