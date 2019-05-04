fff468859b698900f8176e6947a4469ffb5288c7
nakochan <eunnamu094@gmail.com>
2019-05-05 01:06:17 +0900

    fix socket.io emit

--- backend/src/lib/socket.io.js
+++ backend/src/lib/socket.io.js
@@ -1,14 +1,14 @@
 module.exports = {
   start: io => {
     io.on('connection', socket => {
-      socket.on('newTopic', data => io.emit('newTopic', data))
+      socket.on('newTopic', data => io.sockets.emit('newTopic', data))
       socket.on('join', id => socket.join(id))
       socket.on('leave', id => socket.leave(id))
       // socket.on('disconnect', () => {})
     })
   },
-  newBest: (io, title) => io.emit('newBest', { title }),
-  newTopic: (io, title) => io.emit('newTopic', { title }),
+  newBest: (io, title) => io.sockets.emit('newBest', { title }),
+  newTopic: (io, title) => io.sockets.emit('newTopic', { title }),
   newPost: (io, id) => io.sockets.in(id).emit('newPost'),
   vote: (io, id, likes, hates) => io.sockets.in(id).emit('vote', { likes, hates })
 }
