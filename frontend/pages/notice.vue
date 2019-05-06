1c6be2232a0d3638b85574e2389c19700006bb1d
nakochan <eunnamu094@gmail.com>
2019-05-06 14:05:02 +0900

    i-mobile ad test

--- frontend/pages/notice.vue
+++ frontend/pages/notice.vue
@@ -7,13 +7,6 @@
           <el-col :xl='19'>
             <div class='AD'>
               <adsbygoogle ad-slot='1882412178' />
-              <script type="text/javascript">
-                imobile_pid = "63114"; 
-                imobile_asid = "1619629"; 
-                imobile_width = 728; 
-                imobile_height = 90;
-              </script>
-              <script type="text/javascript" src="https://spdeliver.i-mobile.co.jp/script/ads.js?20101001"></script>
             </div>
             <div class='widget-title'>
               <font-awesome-icon icon='bell' />
