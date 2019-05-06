ec1a45d74bac66b634b0e545d6251d803e1ecc78
nakochan <eunnamu094@gmail.com>
2019-05-06 19:51:02 +0900

    update frontend recent list

--- frontend/components/recent.vue
+++ frontend/components/recent.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <div class='widget-title'>
+    <div class='containerSubject'>
       <font-awesome-icon icon='star' />
-      최근 인기글
+      최근 인기 게시물
     </div>
-    <div class='widget-list'>
+    <div class='recentList'>
       <div
         class='item'
+        @click='move(item.id)'
         v-for='(item, index) in topics' :key='index'>
-        <nuxt-link :to='"/b/all/" + item.id'>
-          <span class='star' v-if='item.isBest > 0'>
-            <img :src='item.isBest > 1 ? "/star.svg" : "/burn.svg"'>
-          </span>
-          {{ item.title }}
-          <span class='regdate'>{{ item.created }}</span>
-        </nuxt-link>
+        <span class='star' v-if='item.isBest > 0'>
+          <img :src='item.isBest > 1 ? "/star.svg" : "/burn.svg"'>
+        </span>
+        {{ item.title }}
+        <span class='regdate'>{{ item.created }}</span>
       </div>
     </div>
     <adsbygoogle v-if='domain !== "anime"' />
@@ -37,6 +36,7 @@
     methods: {
       getData: async function() {
         const { data } = await axios.get('/api/topic/list/widget')
+        if (!this.topics) return
         this.topics = data.map(i => {
           i.title = i.title.length > 40 ? i.title.substr(0, 40) + '...' : i.title
           i.created = this.$moment(i.created).format('YYYY/MM/DD HH:mm:ss')
@@ -44,41 +44,47 @@
         })
         return data
       },
+      move(id) {
+        this.$router.push({ path: `/b/all/${id}` })
+      }
     }
   }
 </script>
 
 <style>
-  .widget-list {
+  .recentList {
     line-height: 1.2;
-    text-align: justify;
-    background-color: #F5F5F5;
     margin-bottom: 1rem;
     padding: .5em;
+    border-radius: .25rem;
+    background-color: #F5F5F5;
   }
-  .widget-list .item {
-    background-color: #FFF;
+  .recentList .item {
+    margin-bottom: .5rem;
+    padding: .5rem;
     border: 1px solid rgba(0, 0, 0, 0.1);
     border-radius: 3px;
     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.12);
-    margin-bottom: .5rem;
-    padding: .5rem;
+    background-color: #FFF;
     font-size: .8rem;
-    text-align: left;
   }
-  .widget-list .item:last-child {
+  .recentList .item:last-child {
     margin-bottom: 0;
   }
-  .widget-list .item a {
+  .recentList .item:hover {
+    background: #FAFAFA;
+    cursor: pointer;
+  }
+  .recentList .item a {
     color: #333;
   }
-  .widget-list .item span.star img {
+  .recentList .item span.star img {
     width: 14px;
     height: 14px;
     margin-bottom: 4px;
     vertical-align: middle;
   }
-  .widget-list .item span.regdate {
-    color: #f78989;
+  .recentList .item span.regdate {
+    color: #F78989;
   }
 </style>
