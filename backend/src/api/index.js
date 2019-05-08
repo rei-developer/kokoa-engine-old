08b29440a2926f0eb1d4bed961515f0a3bf4fb98
nakochan <eunnamu094@gmail.com>
2019-05-09 02:26:52 +0900

    fix version

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -6,7 +6,7 @@ const topic = require('./topic')
 
 const app = new Router()
 
-const VERSION = 26
+const VERSION = 27
 
 app.get('/version', ctx => ctx.body = { version: VERSION, status: 'ok' })
 app.use('/auth', auth.routes())
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -42,7 +42,7 @@
     data() {
       return {
         backendVersion: 0,
-        frontendVersion: 26
+        frontendVersion: 27
       }
     },
     beforeMount() {
