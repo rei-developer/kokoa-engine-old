9c130e74f84fc8a2ae469548aaa70975cbc66f7e
nakochan <eunnamu094@gmail.com>
2019-05-11 23:10:11 +0900

    fix bug

--- frontend/assets/lib.js
+++ frontend/assets/lib.js
@@ -7,9 +7,6 @@ module.exports.getBoardName = domain => {
     case 'best':
       name = '인기'
       break
-    case 'ilbe':
-      name = '일베'
-      break
     case 'girl':
       name = '연예'
       break
