d191855242ad6fb2aa3eab96b2f94408304a7cec
nakochan <eunnamu094@gmail.com>
2019-05-14 17:11:45 +0900

    fix bug

---
+++ backend/src/api/chart/controller.js
@@ -0,0 +1,22 @@
+const moment      = require('moment')
+const User        = require('../../lib/user')
+const createChart = require('../../database/chart/createChart')
+const readChart   = require('../../database/chart/readChart')
+
+module.exports.votes = async ctx => {
+  const user = await User.getUser(ctx.get('x-access-token'))
+  if (!user) return
+  let {
+    id,
+    select
+  } = ctx.request.body
+  if (id < 1) return
+  const ip = ctx.get('x-real-ip')
+  const date = await readChart.voted(user.id, id, ip)
+  if (date) {
+    const created = moment(date).format('YYYY/MM/DD HH:mm:ss')
+    return ctx.body = { message: `이미 투표한 게시물입니다. (${created})`, status: 'fail' }
+  }
+  await createChart.votes(user.id, id, select, ip)
+  ctx.body = { status: 'ok' }
+}
