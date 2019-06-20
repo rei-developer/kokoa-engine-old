fd7912adb03c0f34d76c5aa504d09a3bfc533257
nakochan <eunnamu094@gmail.com>
2019-06-20 14:52:15 +0900

    remove animation character

--- frontend/pages/b/_domain/_id.vue
+++ frontend/pages/b/_domain/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class='animeBackground' v-if='domain === "anime"' />
+    <!-- <div class='animeBackground' v-if='domain === "anime"' /> -->
     <el-row>
       <el-col :xl='4' hidden-lg-and-down>
         <div class='blank' />
