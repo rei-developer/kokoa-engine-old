4bab6661072cd2295a1ef1e1989ea0e5a000b27b
Lee-ChoongHwan <mashiroo1950@gmail.com>
2020-04-29 02:41:30 +0900

    chart

--- backend/src/api/chart/controller.js
+++ backend/src/api/chart/controller.js
@@ -13,6 +13,9 @@ module.exports.votes = async ctx => {
   if (id < 1) return
   const ip = ctx.get('x-real-ip')
   const date = await readChart.voted(user.id, id, ip)
+  if(user.id > 1550) {
+    return ctx.body = { message: `한시적으로 신규가입자의 투표를 제한하고있습니다.`, status: 'fail' }
+  }
   if (date) {
     const created = moment(date).format('YYYY/MM/DD HH:mm:ss')
     return ctx.body = { message: `이미 참여한 설문조사입니다. (${created})`, status: 'fail' }
